8233c49f977b5873ecc8e627c18ef450ecd0b866
Maksym Deineka <mdein@softserveinc.com>
2025-10-08 00:12:58 +0300

    Added secondLab changes

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -10,99 +10,107 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            // FOR
-            //for (int i = 0; i < 100; i++)
-            //{ 
-            //    Console.WriteLine(i);
-            //}
+            //////FOR
+            for (int r = 0; r < 100; r++)
+            {
+                Console.WriteLine(r);
+            }
 
-            //Console.ReadLine();
+            // ВКЛАДЕНИЙ ЦИКЛ FOR
+            for (int x = 1; x <= 10; x++)
+            {
+                for (int y = 1; y <= 10; y++)
+                {
+                    Console.WriteLine($"{x} * {y} = {x * y}");
+                }
+                Console.WriteLine();
+            }
 
-            //// WHILE
-            //int i = 5;
-            //while (i < 4)
-            //{
-            //    Console.WriteLine("WHILE: "+ i);
-            //    i++;
-            //}
+            Console.ReadLine();
 
-            //// DO WHILE
-            //do
-            //{
-            //    Console.WriteLine("DO WHILE: " + i);
-            //} while (i < 4);
+            // WHILE
+            int i = 5;
+            while (i <= 4)
+            {
+                Console.WriteLine("WHILE: " + i);
+                i++;
+            }
 
-            //Console.ReadLine();
+            // DO WHILE
+            do
+            {
+                Console.WriteLine("DO WHILE: " + i);
+            } while (i < 4);
+
+            Console.ReadLine();
 
 
-            //// FOREACH
-            //string[] names = { "Maksym", "Andrew", "Tamara" };
+            // FOREACH
+            string[] names = { "Maksym", "Andrew", "Tamara", "Igor" };
 
-            //foreach (string name in names)
-            //{
-            //    Console.WriteLine("Здоров! "+ name);
-            //}
+            foreach (string name in names)
+            {
+                Console.WriteLine("Здоров! " + name);
+            }
 
-            //Console.ReadLine();
+            Console.ReadLine();
 
-            // КЕРУВАННЯ ВИКОНАННЯМ ЦИКЛУ
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    if (i == 2) { continue; }
-            //    if (i % 2 == 0) { continue; }
-            //    if (i == 7) { break; }
+            //КЕРУВАННЯ ВИКОНАННЯМ ЦИКЛУ
+            for (int t = 0; t < 10; t++)
+            {
+                if (t == 2) { continue; }
+                if (t % 2 == 0) { continue; }
+                if (t == 7) { break; }
 
-            //    Console.WriteLine(i);
-            //}
+                Console.WriteLine(i);
+            }
 
-            //Console.ReadLine();
+            Console.ReadLine();
 
-            // ВКЛАДЕНІ ЦИКЛИ
-            //for (int x = 1; x < 10; x++)
-            //{
-            //    for(int y = 1; y <= 10; y++) 
-            //    {
-            //        Console.WriteLine($"{x} x {y} = {x*y}");
-            //    }
-            //    Console.WriteLine();
-            //}
+            //MAСИВИ
 
-            //Console.ReadLine();
+            //оголошення пустого масиву з 10 елементів
+            int[] numbers = new int[10];
+            //Console.WriteLine(numbers[0]);
 
-            // MAСИВИ
-            //int[] numbers = new int[10];
-            ////Console.WriteLine(numbers[0]);
+            //оголошення масиву з 5 елементів
+            int[] numbers2 = new int[] { 3, 4, 3, 4, 0 };
+            //Console.WriteLine(numbers2[0]);
 
-            //int[] numbers2 = new int[] {1,2,3,4,0};
-            ////Console.WriteLine(numbers2[0]);
+            //перебір масиву з FOR
+            for (int w = 0; w < numbers2.Length; w++)
+            {
+                Console.WriteLine(Convert.ToString(numbers2[w])); // 3,4,3,4,0
+            }
 
-            //int[] numbers3 = { 3, 4, 3, 4, 0 };
+            Console.ReadLine();
 
-            ////for (int i = 0; i < numbers.Length; i++)
-            ////{
-            ////    Console.WriteLine(numbers3[i]);
-            ////}
+            //перебір масиву з FOREACH
+            foreach (int number in numbers2)
+            {
+                Console.WriteLine(Convert.ToString(number));
+            }
+            Console.ReadLine();
 
-            //foreach (int number in numbers3)
-            //{
-            //    Console.WriteLine(number);
-            //}
-            //Console.ReadLine();
 
-            //string[] names = { "Masha", "Petro", "Vasil" };
+            //масив з рядків
+            string[] names1 = { "Masha", "Petro", "Vasil" };
 
-            //Person[] people = new Person[24];
+            //масив з об'єктів
+            Person[] people = new Person[];
 
-            // МЕТОДИ ДЛЯ МAСИВІВ
-            //int[] numbers = { 343, 2, 3, 4, 0, 12, 1, 55 };
-            //Console.WriteLine("Довжина масиву " + numbers.Length);
-            //Console.WriteLine("Максимальне значення " + numbers.Max());
-            //Console.WriteLine("Мінімальне значення " + numbers.Min());
-            //Console.WriteLine("Максимальне значення " + numbers.Max());
-            //Console.WriteLine("Розвертаємо масив");
-            //Console.WriteLine("Сортуємо масив");
+            //МЕТОДИ ДЛЯ МAСИВІВ
+            int[] numbers4 = { 343, 2, 3, 4, 0, 12, 1, 55 };
+            Console.WriteLine("Довжина масиву " + numbers4.Length);
+            Console.WriteLine("Максимальне значення " + numbers4.Max());
+            Console.WriteLine("Мінімальне значення " + numbers4.Min());
+            Console.WriteLine("Максимальне значення " + numbers4.Max());
+            Array.Reverse(numbers4);
+            Console.WriteLine("Розвертаємо масив" + numbers4);
+            Array.Sort(numbers4);
+            Console.WriteLine("Сортуємо масив" + numbers4);
 
-            //var ff = Array.Sort(numbers);
+            Console.ReadLine();
         }
     }
 }
